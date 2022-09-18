084b436d802cdd751556669fee94b1e7b31caa48
ganigapetaravali <107092459+ganigapetaravali@users.noreply.github.com>
2022-09-18 22:47:41 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,23 +1,16 @@
-#!/usr/bin/env python
 import unittest
-import app
-
+from app import app
 class TestHello(unittest.TestCase):
-
     def setUp(self):
-        app.app.testing = True
-        self.app = app.app.test_client()
-
+        app.testing = True
+        self.app = app.test_client()
     def test_hello(self):
         rv = self.app.get('/')
         self.assertEqual(rv.status, '200 OK')
         self.assertEqual(rv.data, b'Hello World!\n')
-
-    def test_hello_hello(self):
-        rv = self.app.get('/hello/')
-        self.assertEqual(rv.status, '200 OK')
-        self.assertEqual(rv.data, b'Hello World!\n')
-
+if __name__ == '__main__':
+    unittest.main()
+    
 if __name__ == '__main__':
     import xmlrunner
     runner = xmlrunner.XMLTestRunner(output='test-reports')
